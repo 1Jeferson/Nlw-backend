f0d397b3baa21fbc23057cc18b96a89642de1d09
1Jeferson <jefersonnanet.95@gmail.com>
2024-07-11 10:56:45 -0300

    nlw day three

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -5,6 +5,8 @@ import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
 import nodemailer from 'nodemailer';
 import { dayjs } from "../lib/dayjs";
+import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 
 export async function confirmTrip(app: FastifyInstance) {
@@ -31,11 +33,11 @@ export async function confirmTrip(app: FastifyInstance) {
         })
 
         if (!trip) {
-            throw new Error('Trid not found.')
+            throw new ClientError('Trid not found.')
         }
 
         if (trip.is_confirmed) {
-            return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+            return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
         }
 
         await prisma.trip.update({
@@ -53,7 +55,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
         await Promise.all(
             trip.participants.map(async (participant) => {
-                const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`
+                const confirmationLink = `h${env.API_BASE_URL}/participants/${participant.id}/confirm`
 
 
                 const message = await mail.sendMail({
@@ -83,7 +85,7 @@ export async function confirmTrip(app: FastifyInstance) {
             })
         )
 
-        return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+        return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
     })
 
 }
